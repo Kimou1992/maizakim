b9350f84cf55e9de11dad765f229c844b89c8b9e
Kimou1992 <maizanahhal@gmail.com>
2025-01-08 11:07:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const dbUser = process.env.DB_USER; // الوصول إلى متغير البيئة
+const dbPassword = process.env.DB_PASSWORD; 
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my simple web service!');
+  res.send(`DB User: ${dbUser}, DB Password: ${dbPassword}`);
 });
 
 app.listen(port, () => {

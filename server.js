20593c9c021f962d09ae96f2619970fa71e1cf55
Kimou1992 <maizanahhal@gmail.com>
2025-01-08 11:03:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const { Client } = require('pg');
-
 const app = express();
-const port = process.env.PORT || 3000;
-
-// استخدام رابط ثابت لاختبار الاتصال
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  }
-});
-
-
-client.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL database'))
-  .catch(err => {
-    console.error('❌ Connection error:', err.message);
-    console.error('Error stack:', err.stack);
-  });
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Testing database connection!');
+  res.send('Welcome to my simple web service!');
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });

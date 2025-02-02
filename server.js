0ca16761b5ac75cf06334ffd7ce8bb27e260c0dc
Kimou1992 <maizanahhal@gmail.com>
2025-02-02 11:43:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// تقديم الملفات الثابتة
-app.use(express.static(path.join(__dirname, 'public')));
+// تقديم الملفات الثابتة من المجلد الحالي
+app.use(express.static(__dirname));
 
 // توجيه الصفحة الرئيسية
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // تشغيل الخادم
